4bf06c3d95c5177cd6e6cd20587fd0c794fe4e51
Diego Emerick <diegoemericksousa@hotmail.com>
2017-11-08 10:59:50 -0200

    database migration

---
+++ lawyer-service/database/migrations/2017_11_08_021355_create_lawyers_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateLawyersTable extends Migration
+{
+
+    use SoftDeletes;
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('lawyers', function (Blueprint $table) {
+            $table->increments('id');
+            $table->text('name');
+            $table->text('phone');
+            $table->text('mail');
+            $table->text('document');
+            $table->softDeletes('deleted');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('lawyers');
+    }
+}
