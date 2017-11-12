5d4d0cdd18ed374e91117815f10ac4da6f9586d5
Diego Emerick <diegoemericksousa@hotmail.com>
2017-11-12 19:01:14 -0200

    fix api

--- lawyer-service/routes/api.php
+++ lawyer-service/routes/api.php
@@ -2,8 +2,8 @@
 
 use Illuminate\Http\Request;
 
-Route::get('/v1/lawyers', 'LawyerController@index');
-Route::post('/v1/lawyers', 'LawyerController@create');
-Route::put('/v1/lawyers/{id}', 'LawyerController@update');
-Route::get('/v1/lawyers/{id}', 'LawyerController@get');
-Route::delete('/v1/lawyers/{id}', 'LawyerController@delete');
+Route::get('/v1/lawyer', 'LawyerController@index');
+Route::post('/v1/lawyer', 'LawyerController@create');
+Route::put('/v1/lawyer/{id}', 'LawyerController@update');
+Route::get('/v1/lawyer/{id}', 'LawyerController@get');
+Route::delete('/v1/lawyer/{id}', 'LawyerController@delete');
